c0c270745a5a9c879176c010e3542eac7302cf4d
EdamAme-x <edame8080@gmail.com>
2023-10-20 18:15:48 +0900

    change to ts / change dir / fix style

--- src/utils/import-parser.ts
+++ src/utils/import-parser.ts
@@ -1,4 +1,4 @@
-export function importParser(code) {
+export function importParser(code: string): string {
   code = code.replace(/from\s+[\'\"\`](.+)[^\'\"\`][\'\"\`]/gim, (match, k) => {
     if (!match.includes('//')) {
       match = match.replace(k, 'https://esm.sh/' + k)
