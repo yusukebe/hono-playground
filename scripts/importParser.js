62f27323b0a6ae72d215ebf0fa0577bd12a1db04
EdamAme-x <edame8080@gmail.com>
2023-10-20 16:45:41 +0900

    import parser & fix style

---
+++ scripts/importParser.js
@@ -0,0 +1,14 @@
+export function importParser(code) {
+
+    code = code.replace(/from\s+[\'\"\`](.+)[^\'\"\`][\'\"\`]/, (match, k) => {
+        if (!match.includes("\/\/")) {
+            match = match.replace(k, "https://esm.sh/" + k)
+        }
+
+        return match;
+    })
+
+    console.log(code)
+
+    return code;
+}
